80ac02d82b23290302d297ada35a90750fe5540c
Jonas Persson <jonpe960@gmail.com>
2019-06-23 19:25:21 +0200

    wip

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker run -it -u $(id -u $USER) --rm -e HOSTNAME='asdf' -e KS_ROOT_HASH=$KS_ROOT_HASH -v $(readlink -f .):/work ks-builder:latest
+
+docker run -it -u $(id -u $USER) --rm \
+    -v $(readlink -f .):/work \
+    -v $(readlink -f .)/src/build:/output \
+    -v $(readlink -f .)/src/rootfs/:/input \
+    ks-builder:latest bash -c "make CROSS_COMPILE=aarch64-linux-gnu- -C /work/src && cp /work/src/build/kickstart /work/src/rootfs/ && /create_rootfs "
+
--- src/main.c
+++ src/main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
 
 int main(int argc, char **argv)
 {
+    struct termios ctrl;
+
+    tcgetattr(STDIN_FILENO, &ctrl);
+    ctrl.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &ctrl);
+
     printf ("Kickstart %s starting...\n",VERSION);
 
     while (1)
-        sleep(1);
+    {
+        int c = getc(stdin);
+
+        if (c == 'r')
+        {
+            printf ("Rebooting...\n");
+            reboot(0x1234567);
+        }
+    }
 }
