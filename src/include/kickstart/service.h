cf983270ca2336b2377d31b4c2440cd1f2fe26ed
Jonas Blixt <jonpe960@gmail.com>
2019-11-02 09:38:49 +0000

    Inital work on service concept

--- src/include/kickstart/service.h
+++ src/include/kickstart/service.h
@@ -5,18 +5,6 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-struct ks_service;
-
-struct
-{
-    const char *description;
-    const char *path;
-    // args
-    struct ks_service *parent;
-    struct ks_service *child;
-    struct ks_service *next;
-} ks_service;
-
 enum service_state
 {
     KS_SVC_INIT,
@@ -26,13 +14,26 @@ enum service_state
     KS_SVC_CRASH_HOLDOFF
 };
 
-
 struct
 {
     pid_t pid;
     int restarts;
+    int exit_result;
     enum service_state state;
 } ks_service_status;
 
+struct ks_service;
+
+struct
+{
+    const char *description;
+    const char *path;
+    // args
+    struct ks_service *next;
+} ks_service;
+
+struct ks_service * ks_service_init(void);
+int ks_service_start(struct ks_service *svc);
+int ks_service_stop(struct ks_service *svc);
 
 #endif
