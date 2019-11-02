7be0aef8f3a82c4f7e2ab241552e4d0eb65e17a8
Jonas Blixt <jonpe960@gmail.com>
2019-11-02 09:39:09 +0000

    Initial work on logging concept

---
+++ src/include/kickstart/log.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_KICKSTART_LOG_H_
+#define INCLUDE_KICKSTART_LOG_H_
+
+#include <kickstart/kickstart.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int ks_log_init(void);
+
+#endif  // INCLUDE_KICKSTART_LOG_H_
---
+++ src/log.c
@@ -0,0 +1,6 @@
+#include <kickstart/log.h>
+
+int ks_log_init(void)
+{
+    return KS_OK;
+}
